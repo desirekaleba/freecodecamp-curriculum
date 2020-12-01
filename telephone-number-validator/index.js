2670054e70ea5b24eb9433deb06301b595afa0d1
desirekaleba <desirekaleba@gmail.com>
2020-12-01 23:11:22 +0300

    Change phone number type for testing purpose

--- telephone-number-validator/index.js
+++ telephone-number-validator/index.js
@@ -1,5 +1,7 @@
 function telephoneCheck(str) {
+
+  
   return /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/.test(str);
 }
 
-console.log(telephoneCheck("1 (555) 555-5555"));
+console.log(telephoneCheck("5555555555"));
