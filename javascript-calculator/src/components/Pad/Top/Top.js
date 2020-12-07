278a66a644389309859fe9537a0af8ac54cbf0f0
desirekaleba <desirekaleba@gmail.com>
2020-12-07 23:51:57 +0300

    Add top pad

---
+++ javascript-calculator/src/components/Pad/Top/Top.js
@@ -0,0 +1,10 @@
+const Top = (props) => {
+    return (
+        <div>
+            <button id="clear" value="c" onClick={props.handleInput}>C</button>
+        </div>
+        
+    );
+};
+
+export default Top;
