273010c1f2e74243923ebb4fbab30cb677e25bdc
desirekaleba <desirekaleba@gmail.com>
2020-12-09 20:01:38 +0300

    feat: Add AC button styles
    Top.css file imported

--- javascript-calculator/src/components/Pad/Top/Top.js
+++ javascript-calculator/src/components/Pad/Top/Top.js
@@ -1,7 +1,9 @@
+import './Top.css';
+
 const Top = (props) => {
     return (
         <div>
-            <button id="clear" value="c" onClick={props.handleInput}>C</button>
+            <button id="clear" value="c" onClick={props.handleInput}>AC</button>
         </div>
         
     );
