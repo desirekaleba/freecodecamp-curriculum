8e5486573ccd22628b8e3d144bae4b9aa4498e6c
desirekaleba <desirekaleba@gmail.com>
2020-12-13 10:10:42 +0300

    import CSS file

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,5 +1,5 @@
-const { Component } = require("react");
-
+import React, { Component } from 'react';
+import './App.css';
 class App extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
