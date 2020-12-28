d22ff1f88f101338330eb97d8b37f8564992256b
desirekaleba <desirekaleba@gmail.com>
2020-12-28 20:56:15 +0300

    Add file analyzer endpoint

--- file-metadata/server.js
+++ file-metadata/server.js
@@ -11,7 +11,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function(req, res) {
 
+});
 
 
 const port = process.env.PORT || 3000;
